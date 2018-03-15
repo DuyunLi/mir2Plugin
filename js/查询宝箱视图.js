8ec213f8e9ef0bba658b2c922f5bf0c464befd20
李杜云 <qq664820828@hotmail.com>
2018-03-16 02:34:33 +0800

    update

--- js/查询宝箱视图.js
+++ js/查询宝箱视图.js
@@ -2,7 +2,7 @@ var dataSource = [{idx:1,name:"text",count:1}];
 var ps =[];
 var format = function(data)
 {
-   var ret = "<text:请放入需要打开的宝箱：:80:75{FCOLOR=253}>\";
+   var ret = "<text:请放入需要打开的宝箱：:80:75{FCOLOR=253}>\\";
    var lineStart = [25,-353,-731,-1084];
    var certainlyTop = 60;
    var cxtractTop = 105;
@@ -11,4 +11,6 @@ var format = function(data)
        var line = parseInt( i/9);
        
    }
+   return ret;
 }
+console.log(format(dataSource));
