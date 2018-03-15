641b0043b4fd920c414a8f7d03af4946c9571ec1
duyunli <posuixin@126.com>
2018-03-16 02:48:35 +0800

    同步

---
+++ js/查询宝箱数据视图.js
@@ -0,0 +1,16 @@
+var dataSource = [{idx:1,name:"text",count:1}];
+var ps =[];
+var format = function(data)
+{
+   var ret = "<text:请放入需要打开的宝箱：:80:75{FCOLOR=253}>\\";
+   var lineStart = [25,-353,-731,-1084];
+   var certainlyTop = 60;
+   var cxtractTop = 105;
+   for(var i =0 ; i< data.length; i ++)
+   {
+       var line = parseInt( i/9);
+       
+   }
+   return ret;
+}
+console.log(format(dataSource));
