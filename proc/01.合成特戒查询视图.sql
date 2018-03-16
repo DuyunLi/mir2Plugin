d276235d9b6cd8f66e5319051ead308526d95252
duyun.li <posuixin@126.com>
2018-03-17 00:22:53 +0800

    up

--- proc/01.合成特戒查询视图.sql
+++
@@ -1,5 +0,0 @@
-SELECT resultIdx,
-       resultCount ret
-  FROM synthesis
- WHERE srcIdx = {0} AND 
-       type = 1;
--- proc/08.查询宝箱数据视图.sql
+++
@@ -1,9 +0,0 @@
-SELECT srcIdx,
-       count,
-       type
-  FROM TreasureBox
- WHERE idx = {0} AND 
-       type IN (1, 2) AND 
-       isactive = 1
- ORDER BY idx,
-          sort;
