246c8ff9d1c3413beb0d0b7f1cdb000f70cd8a2e
duyun.li <posuixin@126.com>
2018-03-16 16:27:41 +0800

    修正

---
+++ proc/11.合成特戒材料查询视图.sql
@@ -0,0 +1,8 @@
+SELECT det.srcIdx,
+       det.srcCount
+  FROM SynthesisDetail det
+       JOIN
+       Synthesis syn ON syn.idx = det.synthesisID
+ WHERE syn.srcIdx = {0} AND 
+       syn.isActive = 1 AND 
+       syn.type = 1;
