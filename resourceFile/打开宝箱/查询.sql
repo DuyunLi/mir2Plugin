88c0e0ffd1c0249d375f958c4d92b910179a0af6
duyun.li <posuixin@126.com>
2018-04-02 23:55:48 +0800

    宝箱随机函数修正

--- resourceFile/打开宝箱/查询.sql
+++ resourceFile/打开宝箱/查询.sql
@@ -2,8 +2,8 @@ SELECT srcIdx,
        count,
        type
   FROM TreasureBox
- WHERE idx = {0} AND 
-       type IN (1, 2) AND 
+ WHERE idx = {0} AND  
        isactive = 1
- ORDER BY idx,
+ ORDER BY CASE WHEN type >= 100 THEN 0 ELSE 1 END,
+          type,
           sort;
