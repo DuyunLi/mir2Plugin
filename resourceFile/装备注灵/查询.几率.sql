f4a7f5df705fda44b6fa9455b236dfbf3f23b25c
duyun.li <posuixin@126.com>
2018-04-05 02:36:08 +0800

    1

--- resourceFile/装备注灵/查询.几率.sql
+++ resourceFile/装备注灵/查询.几率.sql
@@ -1,10 +1,11 @@
-SELECT ifnull(sum(CASE WHEN type = 1 THEN probability ELSE 0 END),0) countRate,
-       ifnull(sum(CASE WHEN type = 2 THEN probability ELSE 0 END),0) qualityRate,
+SELECT ifnull(sum(CASE WHEN t.type = 1 THEN t.probability ELSE 0 END), -1) countRate,
+       ifnull(sum(CASE WHEN t.type = 2 THEN t.probability ELSE 0 END), -1) qualityRate,
        {1} / 255 count,
        {1} % 255 level
   FROM (
            SELECT sm.probability,
-                  type
+                  type,
+                  's' t
              FROM soulMaterial sm
             WHERE srcIdx = {0} AND 
                   type = 1 AND 
@@ -12,13 +13,16 @@ SELECT ifnull(sum(CASE WHEN type = 1 THEN probability ELSE 0 END),0) countRate,
                   level = {1} / 255
            UNION ALL
            SELECT sm.probability,
-                  type
+                  type,
+                  's' t
              FROM soulMaterial sm
             WHERE srcIdx = {0} AND 
                   type = 2 AND 
                   isactive = 1 AND 
                   level = {1} % 255
        )
-       t;
+       t
+ GROUP BY t.t
+HAVING sum(t.type) = 3;
 
 ;
