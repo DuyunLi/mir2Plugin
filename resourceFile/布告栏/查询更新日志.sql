78832580f97f35f045bc975b74d27cf316880ed4
duyun.li <posuixin@126.com>
2018-04-24 22:59:35 +0800

    布告栏

--- resourceFile/布告栏/查询更新日志.sql
+++ resourceFile/布告栏/查询更新日志.sql
@@ -1,10 +1,14 @@
-SELECT ID,
-       Title,
-       Text,
-       CreateBy,
-       date(CreatedOn)
-  FROM userInputText
- WHERE status = 1 AND 
-       typeid = 4
- ORDER BY CreatedOn DESC
- LIMIT {0} OFFSET {0} * ({1}-1);
+WITH tsource AS (
+    SELECT t.ID,
+           t.Title,
+           t.Text1,
+           t.CreateBy,
+           strftime('%Y.%m.%d', t.CreatedOn) date
+      FROM userInputText t
+     WHERE typeid = 4
+     ORDER BY CreatedOn DESC
+)
+SELECT '<text:' || Title || ':215:-51&ltFCOLOR=250&gt> <text:[' || date || ']:215:-51&ltFCOLOR=7&gt>\\' || Text1 text
+  FROM tsource;
+
+ LIMIT {0} OFFSET {0} * ({1} - 1);
