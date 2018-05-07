8b99708233b04b3deab371641bc617c2821473df
duyun.li <posuixin@126.com>
2018-05-08 01:33:45 +0800

    1

--- resourceFile/转身系统/查询天赋属性.sql
+++ resourceFile/转身系统/查询天赋属性.sql
@@ -1,11 +1,28 @@
+WITH tsource AS (
+    SELECT attr.type,
+           attr.valueType,
+           attr.level,
+           attr.LevelType
+      FROM HumanNewLevel hul
+           JOIN
+           humanNewLevelAttr attr ON hul.Level = attr.Level AND 
+                                     hul.type = attr.LevelType
+     WHERE userName = '{0}' AND 
+           hul.Type > 0 AND 
+           hul.type < 16
+     GROUP BY attr.type,
+              attr.valueType,
+              attr.level,
+              attr.LevelType
+)
 SELECT attr.type,
        attr.ValueType,
-       Sum(attr.Value) 
-  FROM HumanNewLevel hul
+       sum(value) value
+  FROM tsource t
        JOIN
-       humanNewLevelAttr attr ON hul.Level >= attr.Level AND 
-                                 hul.type = attr.LevelType
- WHERE userName = '{0}' AND 
-       hul.Type >0 and hul.type < 16
+       humanNewLevelAttr attr ON t.type = attr.type AND 
+                                 t.valueType = attr.valueType AND 
+                                 t.LevelType = attr.LevelType AND 
+                                 t.Level >= attr.Level
  GROUP BY attr.type,
           attr.ValueType;
--- resourceFile/转身系统/查询转身属性.sql
+++ resourceFile/转身系统/查询转身属性.sql
@@ -1,11 +1,26 @@
+WITH tsource AS (
+    SELECT attr.type,
+           attr.valueType,
+           max(attr.level) level,
+           attr.LevelType
+      FROM HumanNewLevel hul
+           JOIN
+           humanNewLevelAttr attr ON hul.Level >= attr.Level AND 
+                                     hul.type = attr.LevelType
+     WHERE userName = '{0}' AND 
+           hul.Type= 0
+     GROUP BY attr.type,
+              attr.valueType,
+              attr.LevelType
+)
 SELECT attr.type,
        attr.ValueType,
-       Sum(attr.Value) 
-  FROM HumanNewLevel hul
+       sum(value) value
+  FROM tsource t
        JOIN
-       humanNewLevelAttr attr ON hul.Level >= attr.Level AND 
-                                 hul.type = attr.LevelType
- WHERE userName = '{0}' AND 
-       hul.Type = {1}
+       humanNewLevelAttr attr ON t.type = attr.type AND 
+                                 t.valueType = attr.valueType AND 
+                                 t.LevelType = attr.LevelType AND 
+                                 t.Level >= attr.Level
  GROUP BY attr.type,
           attr.ValueType;
