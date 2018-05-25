cc98616d0506798f841122b1c333009061f6260d
duyun.li <posuixin@126.com>
2018-05-26 00:18:12 +0800

    1

---
+++ resourceFile/活游戏动/查询活动.sql
@@ -0,0 +1,44 @@
+WITH tsource AS (
+    SELECT idx,
+           imgidx,
+           0 fromDate,
+           0 toDate
+      FROM CampaignItems
+     WHERE fromDate = 0
+    UNION ALL
+    SELECT idx,
+           imgidx,
+           fromDate,
+           toDate
+      FROM CampaignItems
+     WHERE fromDate != 0
+    UNION ALL
+    SELECT idx,
+           imgidx,
+           fromDate + 2400,
+           9999
+      FROM CampaignItems
+     WHERE fromDate != 0
+),
+tview AS (
+    SELECT idx,
+           imgidx,
+           CASE WHEN toDate = 0 THEN 0 WHEN t - toDate > 0 THEN t - toDate ELSE toDate - t END value,
+           CASE WHEN toDate = 0 THEN 1 WHEN t < toDate THEN 1 ELSE 0 END type
+      FROM (
+               SELECT idx,
+                      imgidx,
+                      (fromDate + todate) - CAST (strftime('%H%M', time(CURRENT_TIMESTAMP, 'localtime') ) AS INT) + ( (CAST (CAST (strftime('%H%M', time(CURRENT_TIMESTAMP, 'localtime') ) AS INT) / 100 AS INT) - CAST ( (fromDate + todate) / 100 AS INT) ) * 40) t,
+                      toDate
+                 FROM tsource
+                WHERE (fromDate = 0 AND 
+                       toDate = 0) OR 
+                      (fromDate + todate) - CAST (strftime('%H%M', time(CURRENT_TIMESTAMP, 'localtime') ) AS INT) > 0
+                ORDER BY fromDate,
+                         idx
+                LIMIT 5
+           )
+           t
+)
+SELECT *
+  FROM tview;
